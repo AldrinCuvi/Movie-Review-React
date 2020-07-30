4b073e9e45f4d12c1e805e97d7cbb1056d67ca40
AldrinCuvi <aldrincuvi@hotmail.com>
2020-07-29 23:44:11 -0500

    newMovie commit

--- src/components/NewMovie.jsx
+++ src/components/NewMovie.jsx
@@ -1,37 +1,353 @@
-import React from 'react';
-import { TextField, Button, Paper, Grid, Typography } from '@material-ui/core';
+import React, { useState }from 'react';
+import { TextField, Button, Paper, Grid, Typography, Snackbar } from '@material-ui/core';
 import { Save } from '@material-ui/icons';
+import { Alert } from '@material-ui/lab';
 import { makeStyles } from '@material-ui/core/styles';
-import NavBar from './NavBar'
+import NavBar from './NavBar';
 
-function NewMovie() {
-    
-    const useStyles = makeStyles((theme) => ({
-        button: {
-          margin: theme.spacing(1),
-        },
+const useStyles = makeStyles((theme) => ({
+    button: {
+      margin: theme.spacing(1),
+    },
 
-        root: {
-            flexGrow: 1
-        },
-        paper: {
-            padding: theme.spacing(3),
-            margin: 'auto',
-            maxWidth: 500,
-            minWidth: 200,
-            minHeight: 600
-        },
+    root: {
+        flexGrow: 1
+    },
+    paper: {
+        padding: theme.spacing(3),
+        margin: 'auto',
+        maxWidth: 500,
+        minWidth: 200,
+        minHeight: 600
+    },
 
-    }));
-    
+}));
+
+const apiUrl = "https://cors-anywhere.herokuapp.com/https://api-repuesto.herokuapp.com/movies/";
+
+const NewMovie = () => {
+
     const classes = useStyles();
 
+    const [errors, setErrors] = useState({
+        nombre: {
+          isError: false,
+          msg: "",
+        },
+        sinopsis: {
+          isError: false,
+          msg: "",
+        },
+        actores: {
+          isError: false,
+          msg: "",
+        },
+        director: {
+          isError: false,
+          msg: "",
+        },
+        duracion: {
+          isError: false,
+          msg: "",
+        },
+        genero: {
+          isError: false,
+          msg: "",
+        },
+        anio: {
+            isError: false,
+            msg: "",
+        },
+        clasificacion: {
+            isError: false,
+            msg: "",
+        },
+        calificacion: {
+            isError: false,
+            msg: "",
+        },
+    });
+
+    const [openSucess, setOpenSuccess] = React.useState(false);
+    const [disableButton, setDisableButton] = React.useState(false);
+    const [openError, setOpenError] = React.useState(false);
+    const [form, setForm] = useState({
+        fields: {
+            nombre: "",
+            sinopsis: "",
+            actores: "",
+            director: "",
+            duracion: "",
+            genero: "",
+            anio: 0,
+            clasificacion: "",
+            calificacion: 0,
+        },
+    });
+
+    const postMovie = async () => {
+        
+        setDisableButton(true);
+
+        const newBody = {
+            nombre_pelicula: form.fields.nombre.trimStart(),
+            sinopsis_pelicula: form.fields.sinopsis.trimStart(),
+            actores_pelicula: form.fields.actores.trimStart(),
+            director_pelicula: form.fields.director.trimStart(),
+            duracion_pelicula: form.fields.duracion.trimStart(),
+            genero_pelicula: form.fields.genero.trimStart(),
+            anio_estreno_pelicula: Number(form.fields.anio),
+            clasificacion_pelicula: form.fields.clasificacion.trimStart(),
+            calificacion: form.fields.calificacion,
+        };
+
+        const api_call = await fetch(apiUrl, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newBody),
+        });
+
+        const result = await api_call.json();
+
+        if (result.status === "OK") {
+            setOpenSuccess(true);
+            setDisableButton(false);
+            setForm({
+              fields: {
+                nombre: "",
+                sinopsis: "",
+                actores: "",
+                director: "",
+                duracion: "",
+                genero: "",
+                anio: 0,
+                clasificacion: "",
+                calificacion: 0,
+              },
+            });
+        } else {
+            setOpenError(true);
+            setDisableButton(false);
+        }    
+    };
+    
+    const handleValidation = (e) => {
+        e.preventDefault();
+        let finalObject = {};
+        if (!form.fields.nombre.match(/^[0-9a-zA-ZÀ-ÿ.,;«»"\u00f1\u00d1\s]{1,100}$/)) {
+            finalObject.nombre = {
+                isError: true,
+                msg: "Ingresa un título valido.",
+            };
+        } else {
+            finalObject.nombre = {
+                isError: false,
+                msg: "",
+            };
+        }
+
+        if (!form.fields.sinopsis.match(/^[0-9a-zA-ZÀ-ÿ.,;«»"\u00f1\u00d1\s]{1,1000000000}$/)) {
+            finalObject.sinopsis = {
+                isError: true,
+                msg: "No debes ingresar caracteres especiales.",
+            };
+        } else {
+            finalObject.sinopsis = {
+                isError: false,
+                msg: "",
+            };
+        }
+
+        if (!form.fields.actores.match(/^[0-9a-zA-ZÀ-ÿ.,;«»"\u00f1\u00d1\s]{1,100}$/)) {
+            finalObject.actores = {
+                isError: true,
+                msg: "No debes ingresar caracteres especiales.",
+            };
+        } else {
+            finalObject.actores = {
+                isError: false,
+                msg: "",
+            };
+        }
+
+        if (!form.fields.director.match(/^[0-9a-zA-ZÀ-ÿ.,;«»"\u00f1\u00d1\s]{1,100}$/)) {
+            finalObject.director = {
+                isError: true,
+                msg: "No debes ingresar caracteres especiales.",
+            };
+        } else {
+            finalObject.director = {
+                isError: false,
+                msg: "",
+            };
+        }
+
+        if (!form.fields.duracion.match(/^[0-9a-zA-ZÀ-ÿ.,;«»"\u00f1\u00d1\s]{1,20}$/)) {
+            finalObject.duracion = {
+                isError: true,
+                msg: "No debes ingresar caracteres especiales.",
+            };
+        } else {
+            finalObject.duracion = {
+                isError: false,
+                msg: "",
+            };
+        }
+
+        if (!form.fields.genero.match(/^[0-9a-zA-ZÀ-ÿ.,;«»"\u00f1\u00d1\s]{1,100}$/)) {
+            finalObject.genero = {
+                isError: true,
+                msg: "No debes ingresar caracteres especiales.",
+            };
+        } else {
+            finalObject.genero = {
+                isError: false,
+                msg: "",
+            };
+        }
+
+        if (!form.fields.anio.toString().match(/^[0-9]{1900,2030}$/)) {
+            finalObject.anio = {
+                isError: true,
+                msg: "Debes ingresar un año mayor a 1900.",
+            };
+        } else {
+            finalObject.anio = {
+                isError: false,
+                msg: "",
+            };
+        }
+
+        if (!form.fields.clasificacion.match(/^[0-9a-zA-ZÀ-ÿ.,;«»"\u00f1\u00d1\s]{1,10}$/)) {
+            finalObject.clasificacion = {
+                isError: true,
+                msg: "No debes ingresar caracteres especiales.",
+            };
+        } else {
+            finalObject.clasificacion = {
+                isError: false,
+                msg: "",
+            };
+        }
+
+        if (!form.fields.calificacion.toString().match(/^[0-9]{0,11}$/)) {
+            finalObject.calificacion = {
+                isError: true,
+                msg: "No debes ingresar caracteres especiales.",
+            };
+        } else {
+            finalObject.calificacion = {
+                isError: false,
+                msg: "",
+            };
+        }
+
+        if (
+            finalObject.nombre.isError ||
+            finalObject.sinopsis.isError ||
+            finalObject.actores.isError ||
+            finalObject.director.isError ||
+            finalObject.duracion.isError ||
+            finalObject.genero.isError ||
+            finalObject.anio.isError ||
+            finalObject.clasificacion.isError||
+            finalObject.calificacion.isError
+            ) {
+            setErrors({
+                ...errors,
+                ...finalObject,
+            });
+            } else {
+            setErrors({
+                nombre: {
+                    isError: false,
+                    msg: "",
+                    },
+                    sinopsis: {
+                    isError: false,
+                    msg: "",
+                    },
+                    actores: {
+                    isError: false,
+                    msg: "",
+                    },
+                    director: {
+                    isError: false,
+                    msg: "",
+                    },
+                    duracion: {
+                    isError: false,
+                    msg: "",
+                    },
+                    genero: {
+                    isError: false,
+                    msg: "",
+                    },
+                    anio: {
+                        isError: false,
+                        msg: "",
+                    },
+                    clasificacion: {
+                        isError: false,
+                        msg: "",
+                    },
+                    calificacion: {
+                        isError: false,
+                        msg: "",
+                    },
+            });
+
+            postMovie();
+        }
+    };
+
+    const handleChange = (e) => {
+        const previousValues = {
+            fields: { ...form.fields, [e.target.name]: e.target.value },
+        };
+        setForm(previousValues);
+    };
+
+    const handleCloseSuccess = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+    }
+        setOpenSuccess(false);
+    };
+    
+    const handleCloseError = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
+        setOpenError(false);
+    };
+
     return (
-        <div>
+        <React.Fragment>
             <NavBar/>
             <div className={classes.root} style={{padding:'30px'}}>
+                <Snackbar
+                    open={openSucess}
+                    autoHideDuration={6000}
+                    onClose={handleCloseSuccess}
+                >
+                    <Alert onClose={handleCloseSuccess} severity="success">
+                        Guardado con éxito.
+                    </Alert>
+                </Snackbar>
+                <Snackbar
+                    open={openError}
+                    autoHideDuration={6000}
+                    onClose={handleCloseError}
+                >
+                    <Alert onClose={handleCloseError} severity="error">
+                        Algo salió al guardar tu reseña :(
+                    </Alert>
+                </Snackbar>
                 <Paper className={classes.paper} elevation={3}>
-                    <form noValidate>
+                    <form  onSubmit={handleValidation} noValidate>
                         <Grid container spacing={1} direction="column">
                             <Typography style={{color:'#404040', textAlign:'center'}}>
                                 <h2>Introduce la información de la película:</h2>
@@ -45,6 +361,7 @@ function NewMovie() {
                                     variant="outlined"
                                     fullWidth
                                     required
+                                    onChange={handleChange}
                                 />
                             </Grid>
                             <Grid item>
@@ -56,6 +373,7 @@ function NewMovie() {
                                     variant="outlined"
                                     required
                                     fullWidth
+                                    onChange={handleChange}
                                 />
                             </Grid>
                             <Grid item>
@@ -67,6 +385,7 @@ function NewMovie() {
                                     variant="outlined"
                                     required
                                     fullWidth
+                                    onChange={handleChange}
                                 />
                             </Grid>
                             <Grid item>
@@ -78,6 +397,7 @@ function NewMovie() {
                                     variant="outlined"
                                     required
                                     fullWidth
+                                    onChange={handleChange}
                                 />
                             </Grid>
                             <Grid item>
@@ -89,6 +409,7 @@ function NewMovie() {
                                     variant="outlined"
                                     required
                                     fullWidth
+                                    onChange={handleChange}
                                 />
                             </Grid>
                             <Grid item>
@@ -100,6 +421,7 @@ function NewMovie() {
                                     variant="outlined"
                                     required
                                     fullWidth
+                                    onChange={handleChange}
                                 />
                             </Grid>
                             <Grid item>
@@ -111,6 +433,7 @@ function NewMovie() {
                                     variant="outlined"
                                     required
                                     fullWidth
+                                    onChange={handleChange}
                                 />
                             </Grid>
                             <Grid item>
@@ -122,6 +445,7 @@ function NewMovie() {
                                     variant="outlined"
                                     required
                                     fullWidth
+                                    onChange={handleChange}
                                 />   
                             </Grid>
                             <Grid item>
@@ -133,6 +457,7 @@ function NewMovie() {
                                     variant="outlined"
                                     required
                                     fullWidth
+                                    onChange={handleChange}
                                 />
                             </Grid>
                             <Grid item container justify="center">
@@ -147,13 +472,11 @@ function NewMovie() {
                                 </Button>
                             </Grid>
                         </Grid>
-
-
                     </form>
                 </Paper>
             </div>
-        </div>
+        </React.Fragment>
     );
-}
+};
  
 export default NewMovie;
